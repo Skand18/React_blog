45be6b804f2a5cd2b7e431944d813ed0777786a4
Skand Kumar <skandk61@gmail.com>
2022-09-14 17:08:10 +0530

    initial commit

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React from "react";
+import "./sidebar.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          className="img"
+          src="https://images.unsplash.com/photo-1558959703-78d19f4ffb7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&w=1000&q=80"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad doloribus,
+          non sequi totam ex illo blanditiis aliquam consectetur odit facere
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+              <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+
+        <div className="sidebarItem">
+          <span className="sidebarTitle">FOLLOW US</span>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
